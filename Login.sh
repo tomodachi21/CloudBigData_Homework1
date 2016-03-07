3623f61b968222feae8431694ca469ef52ee3f6f
tomodachi21 <ericjohnson@gmail.com>
2016-03-06 23:50:57 -0500

    Delete Login.sh

--- Login.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Login to my AWS Instance
-
-ip=$(wget http://ipinfo.io/ip -qO -)
-ip=$ip$"/32"
-echo $"You are loggin in from IP: "$ip
-aws ec2 authorize-security-group-ingress --group-name AWS-SecGroup --protocol tcp --port 22 --cidr $ip &> /dev/null &
-echo
-ssh -i "efj2106-Test.pem" ubuntu@ec2-52-35-80-198.us-west-2.compute.amazonaws.com
